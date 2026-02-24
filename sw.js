f3c84b85b4d4e385b376028a2fd3a1ad53ba2985
valencia-col <yolaimer@gmail.com>
2026-02-24 18:15:03 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
 const CACHE_NAME = 'macacol-v1';
-const ASSETS = ['index.html', 'chimpance.jpg', 'manifest.json'];
+const ASSETS = [
+  './',
+  './index.html',
+  './chimpance.jpg',
+  './manifest.json'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
 });
